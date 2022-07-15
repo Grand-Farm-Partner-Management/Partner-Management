5f602872a47a1b7226aabf70ff33ad4428c79a09
Philippe <kamokamophilippe13@gmail.com>
2022-07-15 14:39:38 -0500

    project router

--- server/server.js
+++ server/server.js
@@ -7,9 +7,10 @@ const app = express();
 const sessionMiddleware = require('./modules/session-middleware');
 const passport = require('./strategies/user.strategy');
 
+
 // Route includes
 const userRouter = require('./routes/user.router');
-
+const projectRouter = require('./routes/project.router');
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,6 +24,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/project', projectRouter);
 
 // Serve static files
 app.use(express.static('build'));
