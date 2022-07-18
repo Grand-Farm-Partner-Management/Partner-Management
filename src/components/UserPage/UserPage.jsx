7032ee12099a2497973e168c2effbd3948de4eb0
Levi <levikeithmastrud@gmail.com>
2022-07-18 14:16:04 -0500

    imported grand farm fonts

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
-import GrandFarm from '../../../src/images/GrandFarm_Logo .png'
+import GrandFarm from '../../../src/images/GrandFarm_Logo.png'
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
