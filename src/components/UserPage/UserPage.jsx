3da634190ce8bbe496a38a27d5b9bd4602ddc097
Levi <levikeithmastrud@gmail.com>
2022-07-18 14:07:49 -0500

    added color pallette and grand farm image

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import GrandFarm from '../../../src/images/GrandFarm_Logo .png'
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   console.log(user)
   return (
-    <div className="container">
+    <div className="wrapper">
+      <img className = 'GrandFarm' src={GrandFarm}/>
       <h2>Welcome, {user.first_name}!</h2>
-      <p>Your ID is: {user.id}</p>
-      <LogOutButton className="btn" />
+      <button>View Projects</button>
     </div>
   );
 }
