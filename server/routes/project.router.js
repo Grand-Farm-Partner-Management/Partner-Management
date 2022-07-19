7734161ab1b66f1d8db0a6a3f62c008540abc6e0
Brian Campbell <bry123ftw@gmail.com>
2022-07-19 10:54:04 -0500

    delete works, deletes from 2 joins and tasks table and project table

--- server/routes/project.router.js
+++ server/routes/project.router.js
@@ -9,11 +9,11 @@ const router = express.Router();
 router.get('/', (req, res) => {
     const query = `select project.id, project.title, project.description, project.due_time, project.progression, project.completed, project.completed_time from project
     join project_employee on project.id = project_employee.project_id
-    join "user" on project_employee.employee_id = "user".id
-    where "user".id = $1;`
+    join "user" on project_employee.employee_id = "user".id;`
+    //--where "user".id = $1;`
 
-    pool.query(query, [req.user.id])
-    .then (result => {
+    pool.query(query//, [req.user.id])
+    ).then (result => {
         res.send(result.rows);
     })
     .catch((error) => {
@@ -91,38 +91,38 @@ router.get('/:id', (req, res) => {
         });
 });
 
-//delete project... needs 4 statements for each table it is in.
-router.delete('/:id', (req, res) =>{
-    const query1 = `delete from company_project where project.id = $1`; // delete project from company_project join table
-    const query2 = `delete from project_employee where project.id = $1`;// delete project from project_employee join table
-    const query3 = `delete from tasks where project.id = $1`; // delete project tasks from tasks table
+//delete project... needs 4 statements for each table it is in.?
+router.delete('/:id/delete', (req, res) =>{
+    const query1 = `delete from company_project where project_id = $1`; // delete project from company_project join table
+    const query2 = `delete from project_employee where project_id = $1`;// delete project from project_employee join table
+    const query3 = `delete from tasks where project_id = $1`; // delete project tasks from tasks table
     const query4 = `delete from project where project.id = $1`; // delete the project from the project table
     
     pool.query(query1, [req.params.id])
     .then((response) => res.sendStatus(200))
     .catch(error => {
-        console.log(`Error deleting run`, error);
+        console.log(`Error deleting company_project table`, error);
         res.sendStatus(500);
     });
     
     pool.query(query2, [req.params.id])
     .then((response) => res.sendStatus(200))
     .catch(error => {
-        console.log(`Error deleting run`, error);
+        console.log(`Error deleting project_employee table`, error);
         res.sendStatus(500);
     });
     
     pool.query(query3, [req.params.id])
     .then((response) => res.sendStatus(200))
     .catch(error => {
-        console.log(`Error deleting run`, error);
+        console.log(`Error deleting tasks table`, error);
         res.sendStatus(500);
     });
     
     pool.query(query4, [req.params.id])
     .then((response) => res.sendStatus(200))
     .catch(error => {
-        console.log(`Error deleting run`, error);
+        console.log(`Error deleting project table`, error);
         res.sendStatus(500);
     });
 })
