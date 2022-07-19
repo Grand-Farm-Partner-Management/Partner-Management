46d3bfada96c13f42c10bcdcd0dfd06be2092add
Levi <levikeithmastrud@gmail.com>
2022-07-19 10:18:13 -0500

    got a good start on projects page

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import GrandFarm from '../../../src/images/GrandFarm_Logo.png'
 import { Button } from 'reactstrap';
+import { useHistory } from 'react-router-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 function UserPage() {
+  const history = useHistory();
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   console.log(user)
@@ -12,7 +14,7 @@ function UserPage() {
     <div className="wrapper">
       <img className='GrandFarm' src={GrandFarm} />
       <h2 className='welcome'>Welcome, {user.first_name}!</h2>
-      <Button color="primary">View Projects</Button>
+      <Button onClick={() => history.push('/projects')} color="primary">View Projects</Button>
     </div>
   );
 }
