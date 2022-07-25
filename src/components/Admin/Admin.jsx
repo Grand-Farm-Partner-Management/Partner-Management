47cb9b8a94621ca631582dca386b460bf088aa5a
Brian Campbell <bry123ftw@gmail.com>
2022-07-25 17:20:05 -0500

    added a collapsible component to display un assigned employees

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import UnassignedList from './UnassignedList';
+import { Collapse, Button, CardBody, Card, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-function AdminPage() {
+
+function AdminPage(args) {
     /* 
     - make a view for freshly created companies to be accepted or deleted
     - make a view of new employees and the ability to reassign them to companies, as wel as adjust levels
@@ -12,6 +14,9 @@ function AdminPage() {
     const dispatch = useDispatch();
     const unassigned = useSelector((store) => store.unassigned);
     const newPartners = useSelector((store) => store.newPartner);
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+
 
     console.log('unassigned are:', unassigned);
     console.log('new partners are: ', newPartners);
@@ -40,45 +45,16 @@ function AdminPage() {
 
     return (
         <div>
-            <h4>{JSON.stringify(unassigned)}</h4>
-
-
-            {/* {unassigned.map((unassign, i) => (
-                    <h3 key={unassign.id}> 
-                    {unassign.first_name}, 
-                    {unassign.last_name},
-                    {unassign.email}) 
-                    </h3>
-                ))} */}
-
-
-            {/* {unassigned.map((unassign) => (
-
-                    <h3 key={unassign.id}> 
-                    {unassign.first_name}, 
-                    {unassign.last_name},
-                    {unassign.email} 
-                    </h3>
-                ))} */}
-
-            {
-                unassigned.map((unassign) => {
-                    return (
-                        // <h1>hi</h1>
-                        <UnassignedList unassign = {unassign} />
-                    )
-                })
-            }
-
-
-            // {/* {unassigned.length > 0 ? unassigned.map((unassign) => {
-            //         return (
-            //             <UnassignedList unassign={unassign} />
-            //         )
-            //     })
-            //         :
-            //         <h3>"loading"</h3>} */}
-
+            <h1 onClick={toggle} className='links'>Unassigned Employees</h1>
+            <Collapse isOpen={isOpen} {...args} >
+                {
+                    unassigned.map((unassign) => {
+                        return (
+                            <UnassignedList unassign={unassign} />
+                        )
+                    })
+                }
+            </Collapse>
         </div>
     )
 }
--- src/components/Admin/UnassignedList.jsx
+++ src/components/Admin/UnassignedList.jsx
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
+import { Collapse, Button, CardBody, Card, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
 
 function UnassignedList(props) {
-    // const [company, setCompany] = useState(unassign.company_id)
     //console.log('in unassigned list', unassign);
-    let unassign = props.unassign;
+    const unassign = props.unassign;
+    const [company, setCompany] = useState(unassign.company_id)
+    const [isOpen,setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+    const dispatch = useDispatch();
+    
     return (
         <div >
-            <li>{unassign.first_name} {unassign.last_name} {unassign.email} </li>
+            <h4>{unassign.first_name} {unassign.last_name} {unassign.email} </h4>
             <button>Add</button>
         </div>
     )
